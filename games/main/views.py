d686cd23c98eac13cf4d64c725734c0845d7a270
Antonio Macías Zambrano <36481170+antmaczam@users.noreply.github.com>
2020-01-18 17:13:00 +0100

    Añadida la página de inicio

--- games/main/views.py
+++ games/main/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from main.models import Game
 
 # Create your views here.
+
+def inicio(request):
+    games = Game.objects.all()
+    return render(request,'index.html', {'games':games})
+
+def games(request):
+    games = Game.objects.all()
+    return render(request,'games.html',{'games':games})
