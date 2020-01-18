0c115ded5158b0bc341beeed76e72e1b8e6a8cf9
Antonio Macías Zambrano <36481170+antmaczam@users.noreply.github.com>
2020-01-18 16:13:07 +0100

    Preparando .gitignore

--- games/main/admin.py
+++ games/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from main.models import Game
 
 # Register your models here.
+
+admin.site.register(Game)
--- games/main/models.py
+++ games/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator,URLValidator
 
 # Create your models here.
+
+class Game(models.Model):
+    title = models.CharField(max_length=100, unique=True)
+    price = models.FloatField()
+    category = models.TextField()
+    comments = models.PositiveIntegerField()
+    rate = models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    
+    def __str__(self):
+        return self.title
