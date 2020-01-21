c34798ab0f51f353bb912a8c8b4c7c6f73ea8f82
Antonio Macías Zambrano <36481170+antmaczam@users.noreply.github.com>
2020-01-21 20:00:12 +0100

    Eliminada basura de Moi

--- django-games-master/games/main/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from main.models import Game
-
-# Register your models here.
-
-admin.site.register(Game)
--- django-games-master/games/main/tests.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase
-from main.populate import extraerDatosURL
-
-# Create your tests here.
-
-extraerDatosURL()
