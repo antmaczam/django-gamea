b48eaf2cae82787b05c2780313268f1354c8f9cb
moi97 <36935839+moi97@users.noreply.github.com>
2020-01-21 20:51:40 +0100

    Add files via upload

--- games/main/populate.py
+++ games/main/populate.py
@@ -18,14 +18,16 @@ def extraerDatosURL():
         if(rate == ' —'):
             rate = 0.0
         category = game.find('div',class_=["search-results-row-game-infos"]).get_text().split("-")[1]
-        link = game.find('a',class_=["search-results-row-link"]).get('href')
-        pc = urlopen(link).read()
+        link2 = game.find('a',class_=["search-results-row-link"]).get('href')
+        pc = urlopen(link2).read()
         soup = BeautifulSoup(pc, 'lxml')
+        link = soup.find('div', class_=["offers-table-row-cell buy-btn-cell"]).find('a').get('href')
+        picture = soup.find('div', class_=["content-box"]).find('img').get('src')
         comment = soup.find_all('h3', class_=["content-box-title"])
         comments = comment[0].get_text().split()[0]
         if(comments == 'Be'):
             comments = 0
-        Game.objects.create(title=name,price=float(price.split('€')[0]),category=category,comments=int(comments),rate=float(rate),plataform='PC',gamePrice=0)
+        Game.objects.create(title=name,price=float(price.split('€')[0]),category=category,comments=int(comments),rate=float(rate),plataform='PC',link=link,picture=picture,gamePrice=0)
  
     #XBox Games --------------------------------------------------------------------------------------------------------------
 
@@ -41,9 +43,11 @@ def extraerDatosURL():
         if(rate == ' —'):
             rate = 0.0
         plataform = game.find('div',class_=["search-results-row-game-infos"]).get_text().split("-")[1]
-        link = game.find('a',class_=["search-results-row-link"]).get('href')
-        pc = urlopen(link).read()
+        link2 = game.find('a',class_=["search-results-row-link"]).get('href')
+        pc = urlopen(link2).read()
         soup = BeautifulSoup(pc, 'lxml')
+        link = soup.find('div', class_=["offers-table-row-cell buy-btn-cell"]).find('a').get('href')
+        picture = soup.find('div', class_=["content-box"]).find('img').get('src')
         comment = soup.find_all('h3', class_=["content-box-title"])
         comments = comment[0].get_text().split()[0]
         if(comments == 'Be'):
@@ -53,7 +57,7 @@ def extraerDatosURL():
            category = 'Sin categoria'
         else:
            category = category[0].get_text()
-        Game.objects.create(title=name,price=float(price.split('€')[0]),category=category,comments=int(comments),rate=float(rate),plataform=plataform,gamePrice=0)
+        Game.objects.create(title=name,price=float(price.split('€')[0]),category=category,comments=int(comments),rate=float(rate),plataform=plataform,link=link,picture=picture,gamePrice=0)
    
     #PSX Games -------------------------------------------------------------------------------------------------------------------
 
@@ -69,9 +73,11 @@ def extraerDatosURL():
         if(rate == ' —'):
             rate = 0.0
         plataform = game.find('div',class_=["search-results-row-game-infos"]).get_text().split("-")[1]
-        link = game.find('a',class_=["search-results-row-link"]).get('href')
-        pc = urlopen(link).read()
+        link2 = game.find('a',class_=["search-results-row-link"]).get('href')
+        pc = urlopen(link2).read()
         soup = BeautifulSoup(pc, 'lxml')
+        link = soup.find('div', class_=["offers-table-row-cell buy-btn-cell"]).find('a').get('href')
+        picture = soup.find('div', class_=["content-box"]).find('img').get('src')
         comment = soup.find_all('h3', class_=["content-box-title"])
         comments = comment[0].get_text().split()[0]
         category = soup.find_all('a', rel=['noopener noreferrer'])
@@ -81,7 +87,7 @@ def extraerDatosURL():
            category = 'Sin categoria'
         else:
            category = category[0].get_text()
-        Game.objects.create(title=name,price=float(price.split('€')[0]),category=category,comments=int(comments),rate=float(rate),plataform=plataform,gamePrice=0)
+        Game.objects.create(title=name,price=float(price.split('€')[0]),category=category,comments=int(comments),rate=float(rate),plataform=plataform,link=link,picture=picture,gamePrice=0)
     
     #Nintendo Games-----------------------------------------------------------------------------------------------------------------
     
@@ -91,15 +97,17 @@ def extraerDatosURL():
     gamesNintendo=soup.find_all('li', class_=["search-results-row"])
     
     for game in gamesNintendo:
-        link = game.find('a',class_=["search-results-row-link"]).get('href')
+        link2 = game.find('a',class_=["search-results-row-link"]).get('href')
         name = game.find('h2').get_text()
         price = game.find('div',class_=["search-results-row-price"]).get_text()
         rate = game.find('div',class_=["metacritic d-none d-xl-block"]).get_text()
         if(rate == ' —'):
             rate = 0.0
         plataform = game.find('div',class_=["search-results-row-game-infos"]).get_text().split("-")[1]
-        pc = urlopen(link).read()
+        pc = urlopen(link2).read()
         soup = BeautifulSoup(pc, 'lxml')
+        link = soup.find('div', class_=["offers-table-row-cell buy-btn-cell"]).find('a').get('href')
+        picture = soup.find('div', class_=["content-box"]).find('img').get('src')
         comment = soup.find_all('h3', class_=["content-box-title"])
         category = soup.find_all('a', rel=['noopener noreferrer'])
         comments = comment[0].get_text().split()[0]
@@ -109,4 +117,4 @@ def extraerDatosURL():
            category = 'Sin categoria'
         else:
            category = category[0].get_text()
-        Game.objects.create(title=name,price=float(price.split('€')[0]),category=category,comments=int(comments),rate=float(rate),plataform=plataform,gamePrice=0)
+        Game.objects.create(title=name,price=float(price.split('€')[0]),category=category,comments=int(comments),rate=float(rate),plataform=plataform,link=link,picture=picture,gamePrice=0)
