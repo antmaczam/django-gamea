4a43a3f97a989008e97aa0e0ab1e5c190f8f5102
Antonio Macías Zambrano <36481170+antmaczam@users.noreply.github.com>
2020-01-22 13:06:52 +0100

    Resuelto error Too many request

--- games/main/populate.py
+++ games/main/populate.py
@@ -3,29 +3,30 @@ from urllib.request import Request, urlopen
 from main.models import Game
 
 def extraerDatosURL():
-    
+    time = 50
     #PC Games -------------------------------------------------------------------------------------------------------------
-
-    pc = urlopen('https://www.allkeyshop.com/blog/catalogue/category-pc-games-all/').read()
+    
+    pc = urlopen('https://www.clavecd.es/catalog/category-pc-games-all/').read()
     soup = BeautifulSoup(pc, 'lxml')
-
+    
     gamesPc=soup.find_all('li', class_=["search-results-row"])
         
     for game in gamesPc:
         name = game.find('h2').get_text()
         price = game.find('div',class_=["search-results-row-price"]).get_text()
         rate = game.find('div',class_=["metacritic d-none d-xl-block"]).get_text()
-        if(rate == ' —'):
+        if(rate == '\n\n    —    \n\n\n\n'):
             rate = 0.0
         category = game.find('div',class_=["search-results-row-game-infos"]).get_text().split("-")[1]
         link2 = game.find('a',class_=["search-results-row-link"]).get('href')
-        pc = urlopen(link2).read()
+        
+        pc = urlopen(link2,timeout=time).read()
         soup = BeautifulSoup(pc, 'lxml')
         link = soup.find('div', class_=["offers-table-row-cell buy-btn-cell"]).find('a').get('href')
         picture = soup.find('div', class_=["content-box"]).find('img').get('src')
         comment = soup.find_all('h3', class_=["content-box-title"])
         comments = comment[0].get_text().split()[0]
-        if(comments == 'Be'):
+        if('S' in comments):
             comments = 0
         
         nq = name.replace(" ", "+")
@@ -35,9 +36,12 @@ def extraerDatosURL():
         soup = BeautifulSoup(game, 'lxml')
         
         priceG = soup.find_all('span', class_=["a-price-whole"])
-        priceG = priceG[0].get_text()
-        priceG = priceG.replace(",", ".")
-        Game.objects.create(title=name,price=float(price.split('€')[0]),category=category,comments=int(comments),rate=float(rate),plataform='PC',link=link,picture=picture,gamePrice=float(priceG))
+        if(len(priceG)==0):
+            priceG = 0.0
+        else:
+            priceG = priceG[0].get_text()
+            priceG = priceG.replace(",", ".")
+        Game.objects.create(title=name,price=float(price.split('€')[0]),category=category,comments=int(comments),rate=float(rate),plataform='PC',link=link2,picture=picture,gamePrice=float(priceG))
     
     #XBox Games --------------------------------------------------------------------------------------------------------------
     
@@ -54,7 +58,8 @@ def extraerDatosURL():
             rate = 0.0
         plataform = game.find('div',class_=["search-results-row-game-infos"]).get_text().split("-")[1]
         link2 = game.find('a',class_=["search-results-row-link"]).get('href')
-        pc = urlopen(link2).read()
+        
+        pc = urlopen(link2,timeout=time).read()
         soup = BeautifulSoup(pc, 'lxml')
         link = soup.find('div', class_=["offers-table-row-cell buy-btn-cell"]).find('a').get('href')
         picture = soup.find('div', class_=["content-box"]).find('img').get('src')
@@ -67,11 +72,24 @@ def extraerDatosURL():
            category = 'Sin categoria'
         else:
            category = category[0].get_text()
-        Game.objects.create(title=name,price=float(price.split('€')[0]),category=category,comments=int(comments),rate=float(rate),plataform=plataform,link=link,picture=picture,gamePrice=0)
-   
+        
+        nq = name.replace(" ", "+")
+        enlace = 'https://www.amazon.es/s?k=' + nq +"&i=videogames&__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&ref=nb_sb_noss_1"
+        linkGame = Request(enlace , headers={'User-Agent': 'Mozilla/5.0'})
+        game = urlopen(linkGame).read() 
+        soup = BeautifulSoup(game, 'lxml')
+        
+        priceG = soup.find_all('span', class_=["a-price-whole"])
+        if(len(priceG)==0):
+            priceG = 0.0
+        else:
+            priceG = priceG[0].get_text()
+            priceG = priceG.replace(",", ".")
+        Game.objects.create(title=name,price=float(price.split('€')[0]),category=category,comments=int(comments),rate=float(rate),plataform=plataform,link=link2,picture=picture,gamePrice=float(priceG))
+    
     #PSX Games -------------------------------------------------------------------------------------------------------------------
-
-    psx = urlopen('https://www.allkeyshop.com/blog/catalogue/category-playstation-all/').read()
+    
+    psx = urlopen('https://www.keyforsteam.de/catalogue/category-playstation-all/').read()
     soup = BeautifulSoup(psx, 'lxml')
 
     gamesPsx=soup.find_all('li', class_=["search-results-row"])
@@ -80,28 +98,42 @@ def extraerDatosURL():
         name = game.find('h2').get_text()
         price = game.find('div',class_=["search-results-row-price"]).get_text()
         rate = game.find('div',class_=["metacritic d-none d-xl-block"]).get_text()
-        if(rate == ' —'):
+        if(rate == '\n\n    —    \n\n\n\n'):
             rate = 0.0
         plataform = game.find('div',class_=["search-results-row-game-infos"]).get_text().split("-")[1]
         link2 = game.find('a',class_=["search-results-row-link"]).get('href')
-        pc = urlopen(link2).read()
+        
+        pc = urlopen(link2,timeout=time).read()
         soup = BeautifulSoup(pc, 'lxml')
         link = soup.find('div', class_=["offers-table-row-cell buy-btn-cell"]).find('a').get('href')
         picture = soup.find('div', class_=["content-box"]).find('img').get('src')
         comment = soup.find_all('h3', class_=["content-box-title"])
         comments = comment[0].get_text().split()[0]
         category = soup.find_all('a', rel=['noopener noreferrer'])
-        if(comments == 'Be'):
+        if(comments == 'Sei'):
            comments = 0
         if(len(category) == 0):
            category = 'Sin categoria'
         else:
            category = category[0].get_text()
-        Game.objects.create(title=name,price=float(price.split('€')[0]),category=category,comments=int(comments),rate=float(rate),plataform=plataform,link=link,picture=picture,gamePrice=0)
+        
+        nq = name.replace(" ", "+")
+        enlace = 'https://www.amazon.es/s?k=' + nq +"&i=videogames&__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&ref=nb_sb_noss_1"
+        linkGame = Request(enlace , headers={'User-Agent': 'Mozilla/5.0'})
+        game = urlopen(linkGame).read() 
+        soup = BeautifulSoup(game, 'lxml')
+        
+        priceG = soup.find_all('span', class_=["a-price-whole"])
+        if(len(priceG)==0):
+            priceG = 0.0
+        else:
+            priceG = priceG[0].get_text()
+            priceG = priceG.replace(",", ".")
+        Game.objects.create(title=name,price=float(price.split('€')[0]),category=category,comments=int(comments),rate=float(rate),plataform=plataform,link=link2,picture=picture,gamePrice=float(priceG))
     
     #Nintendo Games-----------------------------------------------------------------------------------------------------------------
     
-    nintendo = urlopen('https://www.allkeyshop.com/blog/catalogue/category-nintendo-all/').read()
+    nintendo = urlopen('https://cheapdigitaldownload.com/catalog/category-nintendo-all/').read()
     soup = BeautifulSoup(nintendo, 'lxml')
 
     gamesNintendo=soup.find_all('li', class_=["search-results-row"])
@@ -111,10 +143,11 @@ def extraerDatosURL():
         name = game.find('h2').get_text()
         price = game.find('div',class_=["search-results-row-price"]).get_text()
         rate = game.find('div',class_=["metacritic d-none d-xl-block"]).get_text()
-        if(rate == ' —'):
+        if(rate == '\n\n    —    \n\n\n\n'):
             rate = 0.0
         plataform = game.find('div',class_=["search-results-row-game-infos"]).get_text().split("-")[1]
-        pc = urlopen(link2).read()
+        
+        pc = urlopen(link2,timeout=time).read()
         soup = BeautifulSoup(pc, 'lxml')
         link = soup.find('div', class_=["offers-table-row-cell buy-btn-cell"]).find('a').get('href')
         picture = soup.find('div', class_=["content-box"]).find('img').get('src')
@@ -127,4 +160,17 @@ def extraerDatosURL():
            category = 'Sin categoria'
         else:
            category = category[0].get_text()
-        Game.objects.create(title=name,price=float(price.split('€')[0]),category=category,comments=int(comments),rate=float(rate),plataform=plataform,link=link,picture=picture,gamePrice=0)
+        
+        nq = name.replace(" ", "+")
+        enlace = 'https://www.amazon.es/s?k=' + nq +"&i=videogames&__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&ref=nb_sb_noss_1"
+        linkGame = Request(enlace , headers={'User-Agent': 'Mozilla/5.0'})
+        game = urlopen(linkGame).read() 
+        soup = BeautifulSoup(game, 'lxml')
+        
+        priceG = soup.find_all('span', class_=["a-price-whole"])
+        if(len(priceG)==0):
+            priceG = 0.0
+        else:
+            priceG = priceG[0].get_text()
+            priceG = priceG.replace(",", ".")
+        Game.objects.create(title=name,price=float(price.split('$')[1]),category=category,comments=int(comments),rate=float(rate),plataform=plataform,link=link2,picture=picture,gamePrice=float(priceG))
